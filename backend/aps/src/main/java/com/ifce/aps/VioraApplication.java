f2b346145588a53e4d03ab998b41c2744c7ce62e
VivianKailany <viviankailanymarques@gmail.com>
2026-02-19 19:13:30 -0300

    Estrutura inicial do projeto.

---
+++ backend/aps/src/main/java/com/ifce/aps/VioraApplication.java
@@ -0,0 +1,13 @@
+package com.ifce.aps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VioraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VioraApplication.class, args);
+	}
+
+}
---
+++ backend/aps/src/test/java/com/ifce/aps/VioraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifce.aps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VioraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
